663cc4b08871227300754cc625deb44b6961ddbe
chiq2045 <charles.oroko@outlook.com>
2019-10-02 21:22:03 +0000

    install and use dotenv
    also get ready to user router

--- genealogy.js
+++ genealogy.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
+
 const Koa = require('koa');
 const logger = require('koa');
 const bodyParser = require('koa-body');
+// const Router = require('@koa/router');
 
 const genealogy = new Koa();
+// const router = new Router();
+const { PORT } = process.env;
 
+// router.get('/api/v1/genealogy', showGenealogy);
+// function * showGenealogy(ctx, next) {
+//   ctx.body = genealogyExample;
+//   yield next;
+// }
 genealogy
   .use(logger())
   .use(bodyParser({
@@ -14,4 +24,4 @@ genealogy.use((ctx, next) => {
   ctx.body = 'Welcome to the genealogy page';
 });
 
-genealogy.listen(3007);
+genealogy.listen(PORT);
