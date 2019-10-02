427796ba4ac7b46342cdb8755af5d577ee47d398
chiq2045 <charles.oroko@outlook.com>
2019-10-02 21:24:13 +0000

    initial Person structure

---
+++ tree/Person.js
@@ -0,0 +1,15 @@
+/**
+ * Class representing a person on the tree
+ */
+class Person {
+  /**
+   * @class
+   * @param {string} name - name of person
+   * @property {string} name - name of person
+   */
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+module.exports = Person;
