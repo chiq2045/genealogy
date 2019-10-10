3fe12e288dbf0f32b8b74f0636e9ab2590d870e7
chiq2045 <charles.oroko@outlook.com>
2019-10-10 01:42:22 +0000

    test for more properties
    person should have parents, children, spouses

--- test/Person.test.js
+++ test/Person.test.js
@@ -6,4 +6,22 @@ describe('Person', () => {
 
     expect(person.name).toBeTruthy();
   });
+
+  test('should have a parents field', () => {
+    const person = new Person('Suzu Fujibayashi');
+
+    expect(person.parents).toBe(null);
+  });
+
+  test('should have a children field', () => {
+    const person = new Person('Suzu Fujibayashi');
+
+    expect(person.children).toBe(null);
+  });
+
+  test('should have a spouses field', () => {
+    const person = new Person('Suzu Fujibayashi');
+
+    expect(person.children).toBe(null);
+  });
 });
