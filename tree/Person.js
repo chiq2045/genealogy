46ac53d9c076749e211ee69d9c2389f9597da80f
chiq2045 <charles.oroko@outlook.com>
2019-10-10 01:56:48 +0000

    Person passes all tests

--- tree/Person.js
+++ tree/Person.js
@@ -5,9 +5,15 @@ class Person {
   /**
    * @class
    * @param {string} name - name of person
+   * @property {Array} parents - array of pointers
+   * @property {Array} children - array of pointers
+   * @property {Array} spouses - array of pointers
    */
   constructor(name) {
     this.name = name;
+    this.parents = null;
+    this.children = null;
+    this.spouses = null;
   }
 }
 
